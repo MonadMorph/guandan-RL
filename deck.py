c2d87e26b2af7f4758e5ab769af411e2b3a15916
Monad <yankaic@uchicago.edu>
2025-08-12 21:42:10 +0800

    add check hand and play hand functions

--- deck.py
+++ deck.py
@@ -53,41 +53,89 @@ class PlayerDeck:
                 self.royalflush += [self.orderofRanks[i]] * k
         #Special heart of A
 
-    def single(self):
+    def _single(self):
         return [self.orderofRanks[i] for i in range(15) if self.count[i] >= 1]
 
-    def pair(self):
+    def _pair(self):
         return [self.orderofRanks[i] for i in range(15) if self.count[i] >= 2]
     
-    def three(self):
+    def _three(self):
         return [self.orderofRanks[i] for i in range(13) if self.count[i] >= 3]
     
-    def four(self):    
+    def _four(self):    
         return [self.orderofRanks[i] for i in range(13) if self.count[i] >= 4]
     
-    def five(self):
+    def _five(self):
         return [self.orderofRanks[i] for i in range(13) if self.count[i] >= 5]
     
-    def six(self):
+    def _six(self):
         return [self.orderofRanks[i] for i in range(13) if self.count[i] >= 6]
-
     
-    def flush(self):
+    def _flush(self):
         return [self.orderofRanks[i] for i in range(8) if min(self.count[i:i+5]) >= 1 and self.orderofRanks[i] not in self.royalflush]
     
-    def three_of_pair(self):
+    def _three_of_pair(self):
         return [self.orderofRanks[i] for i in range(10) if min(self.count[i:i+3]) >= 2]
     
-    def two_of_three(self):
+    def _two_of_three(self):
         return [self.orderofRanks[i] for i in range(11) if min(self.count[i:i+2]) >= 3]
     
-    def legal(self):
-        return [self.single(), self.pair(), self.three(), self.four(), self.five(),
-                self.flush(), self.three_of_pair(), self.two_of_three()]
-        
+    def _legal(self):
+        return [self._single(), self._pair(), self._three(), self._three_of_pair(), self._flush(), self._two_of_three(), self._four(), self._five(), self.royalflush, self._six()] #bombs: 11,12,13,14
+    
+    def can_play(self, other_hand):
+        hand = self._legal()
+        if other_hand.type <= 6:
+            for i in range(6):
+                if other_hand.type == i+1:
+                    hand[i] = [card for card in hand[i] if self.orderofRanks.index(card) > self.orderofRanks.index(other_hand.rank)]
+                else: hand[i] = []
+        else: 
+            for i in range(10):
+                if other_hand.type > i+5: hand[i] = []
+                elif other_hand.type == i+5:
+                    hand[i] = [card for card in hand[i] if self.orderofRanks.index(card) > self.orderofRanks.index(other_hand.rank)]
+        return hand
+    
+    def play(self, hand):
+        #Should make sure the hand is legal, won't check here
+        if hand.type >0 and hand.type <= 3:
+            self.count[self.orderofRanks.index(hand.rank)] -= hand.type
+        if hand.type == 4:
+            for i in range(3):
+                self.count[self.orderofRanks.index(hand.rank) + i] -= 2
+        if hand.type == 5 or hand.type == 13:
+            for i in range(5):
+                self.count[self.orderofRanks.index(hand.rank) + i] -= 1
+        if hand.type == 6:
+            for i in range(2):
+                self.count[self.orderofRanks.index(hand.rank) + i] -= 3
+        if hand.type == 11 or hand.type == 12:
+            self.count[self.orderofRanks.index(hand.rank)] -= (hand.type-7)
+        if hand.type == 14:
+            self.count[self.orderofRanks.index(hand.rank)] -= 6
+
+        #check royalflush
+        self.royalflush = [card for card in self.royalflush if min(self.count[self.orderofRanks.index(card):self.orderofRanks.index(card)+5]) >= 1]
+
+#Debug      
+class Hand:
+    def __init__(self, type, rank):
+        self.type = type
+        self.rank = rank
+
+"""   
 test = FrenchDeck()
 test.distribute()
 player = test.players[0]
 print(player.cards)
-print(len(player.cards))
-print(player.royalflush, player.flush())
+print(player.royalflush)
+other_hand = Hand(3, '4')
+print(player.can_play(other_hand))
+"""
+player = PlayerDeck([Card(rank='5', suit='hearts'), Card(rank='Q', suit='diamonds'), Card(rank='4', suit='spades'), Card(rank='7', suit='hearts'), Card(rank='9', suit='hearts'), Card(rank='J', suit='diamonds'), Card(rank='9', suit='spades'), Card(rank='Joker', suit='Red'), Card(rank='7', suit='clubs'), Card(rank='4', suit='diamonds'), Card(rank='Q', suit='spades'), Card(rank='3', suit='spades'), Card(rank='Q', suit='diamonds'), Card(rank='J', suit='clubs'), Card(rank='3', suit='diamonds'), Card(rank='J', suit='hearts'), Card(rank='6', suit='hearts'), Card(rank='4', suit='spades'), Card(rank='K', suit='spades'), Card(rank='3', suit='clubs'), Card(rank='5', suit='clubs'), Card(rank='5', suit='hearts'), Card(rank='8', suit='clubs'), Card(rank='4', suit='clubs'), Card(rank='6', suit='clubs'), Card(rank='8', suit='diamonds'), Card(rank='6', suit='spades')])
+print(player.count, player.royalflush)
+other_hand = Hand(5, '4')
+print(player.can_play(other_hand))
+player.play(Hand(2, '8'))
+print(player.count, player.royalflush)
